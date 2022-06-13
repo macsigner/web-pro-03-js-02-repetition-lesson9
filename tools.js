fa4f59ab6bfe7ca043479d809a9d9c3a565e1c5f
Mac Signer <macsigner@gmx.ch>
2022-06-13 21:06:36 +0200

    Move json parse to tools

--- tools.js
+++ tools.js
@@ -1,23 +1,34 @@
+function errorGet() {
+    alert('Ein Fehler ist aufgetreten!\n' +
+        'Die Daten konnten nicht Empfangen werden!\n' +
+        'Bist Du mit dem Internet verbunden?');
+}
+
+function errorPost() {
+    alert('Ein Fehler ist aufgetreten!\n' +
+        'Daten konnten nicht gesendet werden!\n' +
+        'Bist Du mit dem Internet verbunden?');
+}
 
 export function delegate(selector, fn) {
     return function(e) {
-        if(e.target.matches(selector)) {
+        if (e.target.matches(selector)) {
             fn.call(this, e);
         }
     }
 }
 
-export function get(url, fn) {
+export function get(url, fn, errorFn = errorGet) {
     const request = new XMLHttpRequest();
     request.open('GET', url);
 
-    request.addEventListener('load', fn.bind(this, request));
-
-    request.addEventListener('error', () => {
-        alert('Ein Fehler ist aufgetreten!\n' +
-            'Die Daten konnten nicht Empfangen werden!\n' +
-            'Bist Du mit dem Internet verbunden?');
+    request.addEventListener('load', () => {
+        if (stateIsOK(request)) {
+            fn(JSON.parse(request.responseText));
+        }
     });
+
+    request.addEventListener('error', errorGet);
 
     request.send();
 }
@@ -27,13 +38,13 @@ export function post(url, data, fn) {
     request.open('POST', url);
     request.setRequestHeader('Content-Type', 'application/json');
 
-    request.addEventListener('load', fn.bind(this, request));
-
-    request.addEventListener('error', () => {
-        alert('Ein Fehler ist aufgetreten!\n' +
-            'Daten konnten nicht gesendet werden!\n' +
-            'Bist Du mit dem Internet verbunden?');
+    request.addEventListener('load', () => {
+        if (stateIsOK(request)) {
+            fn(JSON.parse(request.responseText));
+        }
     });
+
+    request.addEventListener('error', errorPost);
 
     request.send(JSON.stringify(data));
 }
